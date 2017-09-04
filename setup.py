fe90c412fa698f4f8b259e741e960e222fbb14de
Jeffrey Slort <mail@jeffreyslort.nl>
2017-09-04 20:59:35 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'hashdex=hashdex.file_checker:cli',
+            'hashdex=hashdex.cli:cli',
         ],
     },
 )
