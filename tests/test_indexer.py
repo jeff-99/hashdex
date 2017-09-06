e8c95e1b2ab4ccc9052702de9c151f54f231bd9c
Jeffrey Slort <mail@jeffreyslort.nl>
2017-09-06 22:00:27 +0200

    added test for Hasher class

--- tests/test_indexer.py
+++ tests/test_indexer.py
@@ -1,6 +1,7 @@
 import sqlite3
-from hashdex.indexer import Indexer, create_connection
+from hashlib import sha1, md5
 from hashdex.files import File
+from hashdex.indexer import Indexer, Hasher, create_connection
 
 
 def test_create_connection():
@@ -28,7 +29,7 @@ def test_indexer_can_add_file(mocker):
     cursor.execute.assert_has_calls([
         mocker.call("INSERT OR IGNORE INTO hashes (sha1_hash, md5_hash) VALUES (?,?)", ("hash1", "hash2")),
         mocker.call("INSERT OR IGNORE INTO files (hash_id, full_path, filename) VALUES (?,?,?)",
-             (1, '~/test.txt', 'test.txt')),
+                    (1, '~/test.txt', 'test.txt')),
     ])
 
     assert connection.commit.called is True
@@ -51,3 +52,19 @@ def test_logging_of_db_exception_on_file_add(mocker):
     indexer.add_file(f)
 
     assert connection.rollback.called is True
+
+
+def test_hasher_hashes_file_content(mocker):
+    mocked_open = mocker.patch("hashdex.indexer.open")
+
+    file_mock = mocker.MagicMock()
+    mocked_open.return_value = file_mock
+
+    file_mock.__enter__.return_value = file_mock
+    file_mock.read.return_value = b"content"
+
+    f = File('~/test.txt', 'test.txt')
+    hasher = Hasher()
+    hashes = hasher.get_hashes(f)
+
+    assert hashes == (sha1(b"content").hexdigest(), md5(b"content").hexdigest())
