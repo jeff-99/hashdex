0df4409b30e4de8d83ce93a8e9f6d7102e67a05f
jeff-99 <j_slort@hotmail.com>
2017-09-24 20:34:21 +0200

    Bump version: 0.4.0 → 0.4.1

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = [
 
 setup(
     name='hashdex',
-    version='0.4.0',
+    version='0.4.1',
     description="A file indexer based on content hashes",
     long_description=readme + '\n\n' + history,
     author="Jeffrey Slort",
